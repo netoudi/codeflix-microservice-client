b06472b00bf3f759b0b84c8f27cb01f1932e2532
netoudi <netoudi@rocketmail.com>
2021-08-05 17:05:57 -0300

    Abertura e fechamento do diálogo de busca usando histórico de navegação

--- src/components/DialogSearch/DialogSearchProvider.tsx
+++ src/components/DialogSearch/DialogSearchProvider.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import DialogSearch from 'components/DialogSearch';
 import DialogSearchContext from 'components/DialogSearch/DialogSearchContext';
+
+import useIsSmallWindow from 'hooks/useIsSmallWindow';
 
 export interface DialogSearchProviderProps {
   children: React.ReactNode;
@@ -10,6 +13,8 @@ export interface DialogSearchProviderProps {
 const DialogSearchProvider: React.FunctionComponent<DialogSearchProviderProps> =
   (props) => {
     const [open, setOpen] = React.useState(false);
+    const history = useHistory<{ show_dialog_search?: boolean }>();
+    const isSmallWindow = useIsSmallWindow();
 
     const actions = React.useMemo(
       () => ({
@@ -18,6 +23,50 @@ const DialogSearchProvider: React.FunctionComponent<DialogSearchProviderProps> =
       }),
       []
     );
+
+    React.useEffect(() => {
+      const { show_dialog_search, ...otherState } =
+        history.location.state || {};
+
+      if (open && !show_dialog_search) {
+        history.push({
+          ...history.location,
+          state: {
+            ...history.location.state,
+            show_dialog_search: true,
+          },
+        });
+      } else {
+        if (open) return;
+
+        history.replace({
+          ...history.location,
+          state: {
+            ...otherState,
+          },
+        });
+      }
+    }, [history, open]);
+
+    React.useEffect(() => {
+      const unregister = history.listen((location, action) => {
+        if (action === 'POP') {
+          const { show_dialog_search } = location.state || {};
+          show_dialog_search ? actions.show() : actions.hide();
+          console.log('POP', show_dialog_search);
+        }
+      });
+
+      return () => {
+        unregister();
+      };
+    }, [actions, history]);
+
+    React.useEffect(() => {
+      if (!isSmallWindow) {
+        actions.hide();
+      }
+    }, [actions, isSmallWindow]);
 
     return (
       <DialogSearchContext.Provider value={actions}>
