2d6c109551e80b7abc7939982b55009b579fc2a3
netoudi <netoudi@rocketmail.com>
2021-08-09 11:27:57 -0300

    Adicionando biblioteca react slick para criar um slider de imagens

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import Slider, { Settings } from 'react-slick';
 
 import Page from 'components/Page';
 import VideoThumbnail from 'components/Video/VideoThumbnail';
 
 import banner from 'static/img/the_matrix_banner.jpg';
 
+import { range } from 'utils';
+
 const Home: React.FunctionComponent = () => {
+  const settings: Settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <Page>
-      <VideoThumbnail ImgProps={{ src: banner }} />
+      <Slider {...settings}>
+        {range(1, 5).map((item) => (
+          <div key={item}>
+            <VideoThumbnail ImgProps={{ src: banner }} />
+          </div>
+        ))}
+      </Slider>
     </Page>
   );
 };
